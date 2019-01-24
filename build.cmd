3221c37ded8cd231388787bd0668209224793917
Zach Moore <Zach.Moore@schneider-electric.com>
2019-01-23 16:34:58 -0800

    Updated build script.

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-if NOT DEFINED BUILD_NUMBER (
+if NOT DEFINED MAJOR_VER (
   set MAJOR_VER=3
 )
 
-if NOT DEFINED BUILD_NUMBER (
+if NOT DEFINED MINOR_VER (
   set MINOR_VER=0
 )
 
@@ -13,7 +13,11 @@ if NOT DEFINED BUILD_NUMBER (
 )
 
 if NOT DEFINED OUTPUT_PATH (
-  set OUTPUT_PATH=%CD%
+  set OUTPUT_PATH="%CD%\output"
 )
 
+REM Create the output path for the NuGet package if it doesn't exist
+if not exist %OUTPUT_PATH% mkdir %OUTPUT_PATH%
+
+REM Create Nuget Package
 call Nuget\nuget.exe pack Nuget\VideoXpertSdk.nuspec -Version %MAJOR_VER%.%MINOR_VER%.%BUILD_NUMBER% -OutputDirectory %OUTPUT_PATH%
